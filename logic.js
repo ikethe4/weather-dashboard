c49db7e1e03cc7196db9ebffcb4975826a04035e
Isaac Arnold <isaac@iaivphotography.com>
2020-10-03 15:48:49 -0500

    pseudocode added for js

---
+++ logic.js
@@ -0,0 +1,12 @@
+//vars needed:
+var citySearched= //text in search box in sidebar;
+var cityDisplay
+var dateDisplay
+var temperature
+var windSpeed
+var uvIndex
+
+//search button click event: citySearched should feed into API search in openweathermaps
+
+
+
