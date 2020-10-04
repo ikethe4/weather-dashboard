7d456a5b3775816ede5f3fc524a1fe457d3a2bd7
Isaac Arnold <isaac@iaivphotography.com>
2020-10-03 20:17:29 -0500

    click event for citySearch added.  City name console logs out.  Slight CSS revisions.

--- logic.js
+++ logic.js
@@ -1,12 +1,19 @@
 //vars needed:
-var citySearched= //text in search box in sidebar;
-var cityDisplay
-var dateDisplay
-var temperature
-var windSpeed
-var uvIndex
+// var citySearched= //text in search box in sidebar;
+// var cityDisplay
+// var dateDisplay
+// var temperature
+// var windSpeed
+// var uvIndex
 
 //search button click event: citySearched should feed into API search in openweathermaps
 
+//search button click registers info then clears
+$("#searchBtn").on("click", function () {  
+    var citySearch = $("#citySearch").val(); 
+    console.log(citySearch);
+    //   citySearch needs a new row added to the sidebar for each submission 
+    $("#citySearch").val("");
 
+})
 
