02d451968e83effd7525cd681ab39e7d357190d4
Isaac Arnold <isaac@iaivphotography.com>
2020-10-05 17:08:56 -0500

    functional ajax call and var updates

--- logic.js
+++ logic.js
@@ -10,10 +10,20 @@
 
 //search button click registers info then clears
 $("#searchBtn").on("click", function () {  
+    var apiKey = "7295765f9a8470ed196df254c5b22bfd"
     var citySearch = $("#citySearch").val(); 
-    console.log(citySearch);
+    // console.log(citySearch);
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+citySearch + "&appid=" + apiKey;
+
     //   citySearch needs a new row added to the sidebar for each submission 
     $("#citySearch").val("");
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(citySearch);
+        console.log(response);
+    })
 
 })
 
